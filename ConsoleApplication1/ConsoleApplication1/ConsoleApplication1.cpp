647cbbac2fb43319c5238fcc0106ed446a0d2e11
Leonardo1712 <leo_gd17@hotmail.com>
2018-04-03 17:45:55 -0500

    Second commit to Git

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,10 +2,21 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
 
+using namespace std;
 
-int _tmain(int argc, _TCHAR* argv[])
+int main(void)
 {
+	int aux;
+	cout<<"Enter a number"<<endl;
+	cin>>aux;
+
+	cout<<"Your number is " <<aux<<endl;
+
+	//system("pause");
 	return 0;
 }
 
